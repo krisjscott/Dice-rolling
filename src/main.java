a24178ae79e6d1dafadcbbe33ceb3decb1f9ce7e
Krish Kumar <krishkumar6566@gmail.com>
2026-04-07 23:47:09 +0530

    Added ASCII diagram for dice

--- src/main.java
+++ src/main.java
@@ -1,7 +1,18 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class main {
+class Main {
+    public static String getDiceFace(int value) {
+        return switch (value) {
+            case 1 -> "+-------+\n|       |\n|   o   |\n|       |\n+-------+";
+            case 2 -> "+-------+\n| o     |\n|       |\n|     o |\n+-------+";
+            case 3 -> "+-------+\n| o     |\n|   o   |\n|     o |\n+-------+";
+            case 4 -> "+-------+\n| o   o |\n|       |\n| o   o |\n+-------+";
+            case 5 -> "+-------+\n| o   o |\n|   o   |\n| o   o |\n+-------+";
+            case 6 -> "+-------+\n| o   o |\n| o   o |\n| o   o |\n+-------+";
+            default -> "Invalid roll";
+        };
+    }
     public static void main(String[] args){
         //DECLARE VARIABLE
         Scanner sc = new Scanner(System.in);
@@ -10,13 +21,19 @@ public class main {
         int total = 0;
 
         System.out.print("Enter the number of dice you want to roll: ");
-        num_of_dice = sc.nextInt();
+        num_of_dice = sc.nextInt();//GET NUMBER OF DICE FROM THE USER
 
-        if(num_of_dice > 0){
+        if(num_of_dice > 0){//CHECK IF NUMBER IS GREATER 0
             for(int i = 0; i< num_of_dice;i++){
+                //ROLL ALL THE DICE
                 int random_num = random.nextInt(1,7);
                 System.out.println("You rolled " + random_num);
+                //DISPLAY ASCII OF DICE
+                System.out.println(getDiceFace(random_num));
+                //GET THE TOTAL
                 total+=random_num;
+
+
             }
             System.out.println("Total sum of dice's rolled is: " + total);
        }
@@ -24,11 +41,6 @@ public class main {
         else {
             System.out.println("Number of dice should be greater than 0!!!");
         }
-        //GET NUMBER OF DICE FROM THE USER
-        //CHECK IF NUMBER IS GREATER 0
-        //ROLL ALL THE DICE
-        //GET THE TOTAL
-        //DISPLAY ASCII OF DICE
         sc.close();
     }
 }
